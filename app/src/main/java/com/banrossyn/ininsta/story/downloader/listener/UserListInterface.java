ded036b03ba5e428de46f121129b635b6d860003
Alpha <manikandan.g132003@gmail.com>
2025-02-10 10:55:25 +0530

    Initial commit

---
+++ app/src/main/java/com/banrossyn/ininsta/story/downloader/listener/UserListInterface.java
@@ -0,0 +1,8 @@
+package com.banrossyn.ininsta.story.downloader.listener;
+
+import com.banrossyn.ininsta.story.downloader.api.model.NodeModel;
+import com.banrossyn.ininsta.story.downloader.api.model.TrayModel;
+
+public interface UserListInterface {
+    void FacebookUserListClick(int i, TrayModel trayModel);
+}
